167088d065683df839283fe1c2b8215c24c3b1f2
Zoltan Kochan <zoltan.kochan@gmail.com>
2016-01-16 23:40:17 +0200

    configuring, documenting, refactoring

--- README.md
+++ README.md
@@ -1,1 +1,69 @@
 # remi-decorate
+
+A remi extension that adds the decorate method to the target
+
+[![Dependency Status](https://david-dm.org/zkochan/remi-decorate/status.svg?style=flat)](https://david-dm.org/zkochan/remi-decorate)
+[![Build Status](https://travis-ci.org/zkochan/remi-decorate.svg?branch=master)](https://travis-ci.org/zkochan/remi-decorate)
+[![npm version](https://badge.fury.io/js/remi-decorate.svg)](http://badge.fury.io/js/remi-decorate)
+[![Coverage Status](https://coveralls.io/repos/zkochan/remi-decorate/badge.svg?branch=master&service=github)](https://coveralls.io/github/zkochan/remi-decorate?branch=master)
+
+
+## Installation
+
+```
+npm i remi-decorate
+```
+
+
+## Usage
+
+Registering the extension
+
+```js
+const Remi = require('remi')
+const remiDecorate = require('remi-decorate')
+
+let remi = new Remi({
+  extensions: [remiDecorate],
+})
+```
+
+Once the remi-decorate extension is registered, the remi plugins can decorate the target app
+
+The `.decorate` method can be used to extend the app's API.
+
+```js
+function plugin(app, opts) {
+  /* The app can be decorated by one property at once */
+  app.decorate('server', 'foo', function() {
+    console.log('foo');
+  });
+
+  /* or by several properties at once */
+  app.decorate('server', {
+    bar: 23,
+    qax: 54
+  });
+}
+
+plugin.attributes = {
+  name: 'plugin',
+}
+
+// the decorations will be available in the other plugins
+function plugin2(app, opts) {
+  console.log(app.foo);
+  console.log(app.bar);
+  console.log(app.qax);
+}
+
+plugin2.attributes = {
+  name: 'plugin2',
+  dependencies: 'plugin',
+}
+```
+
+
+## License
+
+MIT
---
+++ test/index.js
@@ -0,0 +1,88 @@
+'use strict'
+const chai = require('chai')
+const expect = chai.expect
+const plugiator = require('plugiator')
+
+const Remi = require('remi')
+const decorate = require('../')
+
+describe('decorate', function() {
+  let remi
+  let app
+
+  beforeEach(function() {
+    app = {}
+    remi = new Remi({
+      extensions: [decorate],
+    })
+  })
+
+  it('should decorate with a single property', function() {
+    let plugin1 = plugiator.anonymous((app, options) => {
+      app.decorate('server', 'foo', 1)
+      expect(app.foo).to.eq(1)
+      expect(app.root.foo).to.eq(1)
+    })
+    let plugin2 = plugiator.anonymous((app, options) => {
+      expect(app.foo).to.eq(1)
+      expect(app.root.foo).to.eq(1)
+    })
+
+    let plugins = [
+      {
+        register: plugin1,
+        options: {foo: 1},
+      },
+      {
+        register: plugin2,
+      },
+    ]
+
+    return remi.register(app, plugins)
+      .then(() => expect(app.foo).to.eq(1))
+  })
+
+  it('should decorate with with multiple properties', function() {
+    let plugin1 = plugiator.anonymous((app, options) => {
+      app.decorate('server', {
+        foo: 1,
+      })
+      expect(app.foo).to.eq(1)
+      expect(app.root.foo).to.eq(1)
+    })
+    let plugin2 = plugiator.anonymous((app, options) => {
+      expect(app.foo).to.eq(1)
+      expect(app.root.foo).to.eq(1)
+    })
+
+    let plugins = [
+      {
+        register: plugin1,
+        options: {foo: 1},
+      },
+      {
+        register: plugin2,
+      },
+    ]
+
+    return remi.register(app, plugins)
+      .then(() => expect(app.foo).to.eq(1))
+  })
+
+  it('should share the decorated elements through register invocations', function() {
+    let plugin = plugiator.anonymous((app, options) => {
+      app.decorate('server', 'foo', 'bar')
+    })
+
+    return remi
+      .register(app, plugin, {})
+      .then(() => {
+        expect(app.foo).to.eq('bar')
+
+        return remi.register(app, [plugiator.noop()], {})
+      })
+      .then(() => {
+        expect(app.foo).to.eq('bar')
+      })
+  })
+})
