efd914e85782cb93c5fac9baf79c973c25c66416
Zoltan Kochan <zoltan.kochan@gmail.com>
2016-02-13 15:47:19 +0200

    code refactoring

--- index.js
+++ index.js
@@ -3,15 +3,22 @@ const merge = require('merge')
 
 module.exports = function() {
   return (next, target, plugin, cb) => {
-    target.decorate = function(prop, method) {
-      let extension = {}
+    function toExtension(prop, method) {
       if (typeof prop === 'string') {
+        let extension = {}
         extension[prop] = method
-      } else if (typeof prop === 'object') {
-        merge(extension, prop)
-      } else {
-        throw new Error('invalid arguments passed to decorate')
+        return extension
       }
+
+      if (typeof prop === 'object') {
+        return prop
+      }
+
+      throw new Error('invalid arguments passed to decorate')
+    }
+
+    target.decorate = function(prop, method) {
+      let extension = toExtension(prop, method)
 
       merge(this, extension)
       merge(this.root, extension)
